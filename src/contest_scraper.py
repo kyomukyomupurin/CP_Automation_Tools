24a5a8f23603a9d42b36f88107f8bc908d06679b
kyomukyomupurin <murakami1728@gmail.com>
2021-03-15 23:23:57 +0900

    :sparkles: Update

--- src/contest_scraper.py
+++ src/contest_scraper.py
@@ -1,28 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import argparse
-import sys
 from pathlib import Path
 from time import sleep
 
 
-# AtCoder
 def save_sample(problem_id: str):
-    problem_url = contest_url + "/tasks/" + contest_name + '_' + problem_id
-    response = requests.get(problem_url)
-    status = response.status_code
-    if status != 200:
+    response = requests.get(f"{contest_url}/tasks/{contest_name}_{problem_id}")
+    if response.status_code != 200:
         print("Error")
         return
-    print("Status : " + str(response.status_code))
     bs = BeautifulSoup(response.text, "html.parser")
     print(bs.title.get_text())
-
-    for h3_element in bs.find_all("h3"):
-        h3_text = h3_element.get_text()
-        if h3_text.startswith("Sample Input") or h3_text.startswith("Sample Output"):
+    Path(f"{contest_name}/{problem_id.upper()}").mkdir()
+    number = 1
+    for h3_tag in bs.find_all("h3"):
+        h3_text = h3_tag.get_text()
+        if h3_text.startswith("Sample Input"):
             print(h3_text)
-            print(h3_element.next_sibling.get_text())
+            print(h3_tag.next_sibling.get_text())
+            Path(f"{contest_name}/{problem_id.upper()}/input{number}.txt").write_text(h3_tag.next_sibling.get_text())
+        elif h3_text.startswith("Sample Output"):
+            print(h3_text)
+            print(h3_tag.next_sibling.get_text())
+            Path(f"{contest_name}/{problem_id.upper()}/output{number}.txt").write_text(h3_tag.next_sibling.get_text())
+            number += 1
+    Path(f"{contest_name}/{problem_id.upper()}/task{problem_id.upper()}.cc").touch()
+    Path(f"{contest_name}/{problem_id.upper()}/Makefile").touch()
 
 
 if __name__ == "__main__":
@@ -31,10 +35,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     contest_url = args.contest_url
     contest_name = contest_url.split('/')[-1]
-    print("Contest Name : " + contest_name)
+    print(f"Contest : Name : {contest_name}")
+    Path(contest_name).mkdir()
 
     problems_number = 6
 
-    for id in range(problems_number):
-        save_sample(chr(ord('a') + id))
+    for num in range(problems_number):
+        save_sample(chr(ord('a') + num))
         sleep(1.0)
