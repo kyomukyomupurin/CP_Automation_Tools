9e77c8f7f3a7ea0e06e09f06d40b70d83f38a5d5
kyomukyomupurin <murakami1728@gmail.com>
2021-03-28 18:53:17 +0900

    :recycle: Refactor code

--- src/contest_scraper.py
+++ src/contest_scraper.py
@@ -6,43 +6,43 @@ from time import sleep
 import string
 
 
-def save_sample(problem_id: str) -> None:
-    response = requests.get(f"{contest_url}/tasks/{contest_name}_{problem_id}")
+def save_sample(id: str) -> None:
+    response = requests.get(
+        f"https://atcoder.jp/contests/{contest}/tasks/{contest}_{id}")
     if response.status_code != 200:
         print("Error")
         return
     bs = BeautifulSoup(response.text, "html.parser")
     print(bs.title.get_text())
-    Path(f"{contest_name}/{problem_id}").mkdir()
+    Path(f"{contest}/{id}/sample").mkdir(parents=True)
     number = 1
     for h3 in bs.find_all("h3"):
         h3_text: str = h3.get_text()
         if h3_text.startswith("Sample Input"):
             print(h3_text)
             print(h3.next_sibling.get_text())
-            Path(f"{contest_name}/{problem_id}/input{number}.txt").write_text(
+            Path(f"{contest}/{id}/sample/input{number}.txt").write_text(
                 h3.next_sibling.get_text())
         elif h3_text.startswith("Sample Output"):
             print(h3_text)
             print(h3.next_sibling.get_text())
-            Path(f"{contest_name}/{problem_id}/output{number}.txt").write_text(
+            Path(f"{contest}/{id}/sample/output{number}.txt").write_text(
                 h3.next_sibling.get_text())
             number += 1
-    Path(f"{contest_name}/{problem_id}/task{problem_id}.cc").touch()
-    Path(f"{contest_name}/{problem_id}/Makefile").touch()
+    Path(f"{contest}/{id}/task{id}.cc").write_text(Path("template.cc").read_text())
+    Path(f"{contest}/{id}/Makefile").write_text(Path("Makefile").read_text())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("contest_url")
+    parser.add_argument("contest")
     args = parser.parse_args()
-    contest_url: str = args.contest_url
-    contest_name = contest_url.split("/")[-1].replace("-", "_")
-    print(f"{contest_name=}")
-    Path(contest_name).mkdir()
+    contest: str = args.contest
+    print(f"{contest=}")
+    Path(contest).mkdir()
 
     problems_number = 6
 
-    for char in string.ascii_uppercase[:problems_number]:
-        save_sample(char)
+    for problem_id in string.ascii_uppercase[:problems_number]:
+        save_sample(problem_id)
         sleep(1.0)
--- src/sample_checker.py
+++ src/sample_checker.py
@@ -4,22 +4,21 @@ import re
 
 
 def check_sample() -> None:
-    task_id = Path(".").name
-    subprocess.run(["make", f"task{task_id}"])
+    task_id = str(Path.cwd()).split("/")[-1]
     number = 1
     passed = True
-    while Path(f"input{number}.txt").exists():
+    while Path(f"sample/input{number}.txt").exists():
         subprocess.run(f"./task{task_id}", check=True, stdin=Path(
-            f"input{number}.txt").open("r"), stdout=Path(f"answer{number}.txt").open("w"))
+            f"sample/input{number}.txt").open("r"), stdout=Path(f"sample/answer{number}.txt").open("w"))
         print(f"Sample{number}... ", end="")
-        if re.sub(" +", " ", Path(f"output{number}.txt").read_text().replace("\n", " ")).strip() != re.sub(" +", " ", Path(f"answer{number}.txt").read_text().replace("\n", " ")).strip():
+        if re.sub(" +", " ", Path(f"sample/output{number}.txt").read_text().replace("\n", " ")).strip() != re.sub(" +", " ", Path(f"sample/answer{number}.txt").read_text().replace("\n", " ")).strip():
             print("NG")
             print("Input : ")
-            print(Path(f"input{number}.txt").read_text())
+            print(Path(f"sample/input{number}.txt").read_text())
             print("Expected : ")
-            print(Path(f"output{number}.txt").read_text())
+            print(Path(f"sample/output{number}.txt").read_text())
             print("Found : ")
-            print(Path(f"answer{number}.txt").read_text())
+            print(Path(f"sample/answer{number}.txt").read_text())
             passed = False
         else:
             print("OK")
