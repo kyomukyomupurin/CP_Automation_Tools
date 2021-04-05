32f0ff9b5d63705acf6e4b9bf577ab0733f6cbef
kyomukyomupurin <murakami1728@gmail.com>
2021-04-05 23:34:50 +0900

    :recycle: Refactor code

--- src/login.py
+++ src/login.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 login_url = "https://atcoder.jp/login"
 
+
 def login() -> None:
     session = requests.Session()
     response = session.get(login_url)
@@ -12,13 +13,17 @@ def login() -> None:
         return
     bs = BeautifulSoup(response.text, "html.parser")
     token: str = bs.find(attrs={"name": "csrf_token"}).get("value")
-    print(token)    
-    payload = {"username" : "", "password" : "", "csrf_token" : token}
+    print(token)
+    payload = {"username": "",
+               "password": "",
+               "csrf_token": token
+               }
     result = session.post(login_url, data=payload)
     if result.status_code != 200:
         print("Failed...")
     else:
         print("Successfull logged in!")
+
 
 if __name__ == "__main__":
     login()
