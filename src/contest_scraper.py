5f40ccfa6310a45442e44a3450aac227452bfd8d
kyomukyomupurin <murakami1728@gmail.com>
2021-04-19 23:31:47 +0900

    :sparkles: Update : analyze the problem statement

--- src/contest_scraper.py
+++ src/contest_scraper.py
@@ -8,6 +8,8 @@ import shutil
 import subprocess
 from http.cookiejar import LWPCookieJar
 import logging
+from datetime import datetime
+import re
 
 
 COOKIE_SAVE_LOCATION = "cookie.txt"
@@ -22,19 +24,28 @@ def save_sample(id: str) -> None:
     else:
         cookiejar = LWPCookieJar(COOKIE_SAVE_LOCATION)
         cookiejar.load()
-        logging.info(" Loaded an exsisting cookie from \"%s\"", COOKIE_SAVE_LOCATION)
+        if first:
+            logging.info(" Loaded an exsisting cookie from \"%s\"",
+                         COOKIE_SAVE_LOCATION)
+            for cookie in cookiejar:
+                logging.info(" This cookie expires at %s",
+                             datetime.fromtimestamp(float(str(cookie.expires))))
         session.cookies.update(cookiejar)
     response = session.get(problem_url)
     try:
         response.raise_for_status()
     except:
-        logging.error(" HTTP request for \"%s\" failed.", problem_url)
+        logging.error(" HTTP request for [%s] failed.", problem_url)
         return
     bs = BeautifulSoup(response.text, "html.parser")
+    flag_YesNo: bool = False
+    flag_YESNO: bool = False
+    flag_998244353: bool = False
+    flag_1000000007: bool = False
     Path(f"{contest}/{id}/sample").mkdir(parents=True)
     number = 1
     for h3 in bs.find_all("h3"):
-        h3_text: str = h3.get_text()
+        h3_text: str = h3.get_text(strip=True)
         if h3_text.startswith("Sample Input"):
             Path(f"{contest}/{id}/sample/input{number}.txt").write_text(
                 h3.find_next_sibling("pre").get_text())
@@ -42,11 +53,40 @@ def save_sample(id: str) -> None:
             Path(f"{contest}/{id}/sample/output{number}.txt").write_text(
                 h3.find_next_sibling("pre").get_text())
             number += 1
-    shutil.copy(Path(".template/template.cc"),
-                Path(f"{contest}/{id}/task{id}.cc"))
+        elif h3_text == "Output":
+            codes = h3.find_next_sibling("p").find_all("code")
+            vars = h3.find_next_sibling("p").find_all("var")
+            flag_float: bool = "absolute or relative error" in h3.find_next_sibling("p").get_text()
+            pattern = re.compile(r"10\^{-[0-9]}")
+            for code in codes:
+                if code.get_text(strip=True) == "YES":
+                    flag_YESNO = True
+                if code.get_text(strip=True) == "Yes":
+                    flag_YesNo = True
+            for var in vars:
+                if var.get_text(strip=True) in ["998244353", "998,244,353"]:
+                    flag_998244353 = True
+                if "10^9+7" in var.get_text(strip=True).replace(" ", ""):
+                    flag_1000000007 = True
+                if var.get_text(strip=True) in ["1000000007", "1,000,000,007"]:
+                    flag_1000000007 = True
+                if flag_float:
+                    allowable_error: str = var.get_text(strip=True).replace(" ", "").strip("()")
+                    if pattern.fullmatch(allowable_error):
+                        Path(f"{contest}/{id}/allowable_error.txt").write_text(re.split('[{}]', allowable_error)[1])
+    if flag_YesNo:
+        shutil.copy(Path(".template/template_YesNo.cc"), Path(f"{contest}/{id}/task{id}.cc"))
+    elif flag_YESNO:
+        shutil.copy(Path(".template/template_YESNO.cc"), Path(f"{contest}/{id}/task{id}.cc"))
+    elif flag_998244353:
+        shutil.copy(Path(".template/template_998244353.cc"), Path(f"{contest}/{id}/task{id}.cc"))
+    elif flag_1000000007:
+        shutil.copy(Path(".template/template_1000000007.cc"), Path(f"{contest}/{id}/task{id}.cc"))
+    else:
+        shutil.copy(Path(".template/template.cc"), Path(f"{contest}/{id}/task{id}.cc"))
     shutil.copy(Path(".template/Makefile"), Path(f"{contest}/{id}/Makefile"))
     logging.info(" Saved task%s", id)
-    if id == "A":
+    if first:
         subprocess.Popen(["make", "-s", "-C", f"{contest}/{id}", "run"])
 
 
@@ -58,8 +98,10 @@ if __name__ == "__main__":
     contest: str = args.contest
     print(f"{contest=}")
     number_of_tasks = 6
+    first: bool = True
     for task_id in string.ascii_uppercase[:number_of_tasks]:
-        save_sample(task_id)
-        if task_id != string.ascii_uppercase[number_of_tasks - 1]:
+        if not first:
             sleep(1.0)
+        save_sample(task_id)
+        first = False
     Path(f"{contest}/A/taskA").unlink()
