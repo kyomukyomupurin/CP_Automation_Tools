dda0ed954a20ec49b603ebe89441c642467cef1c
kyomukyomupurin <murakami1728@gmail.com>
2021-04-25 17:16:33 +0900

    :sparkles: Update : sample check for float

--- src/sample_checker.py
+++ src/sample_checker.py
@@ -1,36 +1,86 @@
 from pathlib import Path
 import subprocess
-import re
+from math import isclose
+
+
+def is_number(x) -> bool:
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
 
 
 def check_sample() -> None:
     task_id: str = str(Path.cwd()).split("/")[-1]
     number: int = 1
-    passed: bool = True
+    passed_all_samples: bool = True
+    flag_float = Path("allowable_error.txt").exists()
     while Path(f"sample/input{number}.txt").exists():
-        subprocess.run(f"./task{task_id}", check=True, stdin=Path(
-            f"sample/input{number}.txt").open("r"), stdout=Path(f"sample/answer{number}.txt").open("w"))
+        expected: list[str] = Path(
+            f"sample/output{number}.txt").read_text().strip().splitlines()
+        answer: list[str] = Path(
+            f"sample/output{number}.txt").read_text().strip().splitlines()
+        subprocess.run(f"./task{task_id}", check=True, stdin=Path(f"sample/input{number}.txt").open(
+            "r"), stdout=Path(f"sample/answer{number}.txt").open("w"))
         print(f"Sample{number}... ", end="")
-        if re.sub(" +", " ", Path(f"sample/output{number}.txt").read_text().replace("\n", " ")).strip() != re.sub(" +", " ", Path(f"sample/answer{number}.txt").read_text().replace("\n", " ")).strip():
-            print("NG")
-            print("Input : ")
-            print(Path(f"sample/input{number}.txt").read_text())
-            print("Expected : ")
-            print(Path(f"sample/output{number}.txt").read_text())
-            print("Found : ")
-            print(Path(f"sample/answer{number}.txt").read_text())
-            passed = False
+        flag_continue = True
+        if flag_float:
+            tolerance: float = pow(
+                10, int(Path("allowable_error.txt").read_text()))
+            for line_expected, line_answer in zip(expected, answer):
+                if not flag_continue:
+                    break
+                for element_expected, element_answer in zip(line_expected.split(), line_answer.split()):
+                    if is_number(element_expected) and is_number(element_answer):
+                        if not isclose(float(element_expected), float(element_answer), rel_tol=tolerance, abs_tol=tolerance):
+                            print("NG")
+                            print("Input : ")
+                            print(
+                                Path(f"sample/input{number}.txt").read_text())
+                            print("Expected : ")
+                            print(
+                                Path(f"sample/output{number}.txt").read_text())
+                            print("Found : ")
+                            print(
+                                Path(f"sample/answer{number}.txt").read_text())
+                            passed_all_samples = False
+                            flag_continue = False
+                            break
+                    else:
+                        if element_expected != element_answer:
+                            print("NG")
+                            print("Input : ")
+                            print(
+                                Path(f"sample/input{number}.txt").read_text())
+                            print("Expected : ")
+                            print(
+                                Path(f"sample/output{number}.txt").read_text())
+                            print("Found : ")
+                            print(
+                                Path(f"sample/answer{number}.txt").read_text())
+                            passed_all_samples = False
+                            flag_continue = False
+                            break
+            if passed_all_samples:
+                print("OK")
         else:
-            print("OK")
+            for line_expected, line_answer in zip(expected, answer):
+                if line_expected.strip().split() != line_answer.strip().split():
+                    print("NG")
+                    print("Input : ")
+                    print(Path(f"sample/input{number}.txt").read_text())
+                    print("Expected : ")
+                    print(Path(f"sample/output{number}.txt").read_text())
+                    print("Found : ")
+                    print(Path(f"sample/answer{number}.txt").read_text())
+                    passed_all_samples = False
+                    break
+            if passed_all_samples:
+                print("OK")
         number += 1
-    if passed:
-        print("Passed all sample cases!")
-
-
-    if Path("allowable_error.txt").exists():
-        pass
-    else:
-        pass
+    if passed_all_samples:
+        print(f"Passed all sample cases!")
 
 
 if __name__ == "__main__":
