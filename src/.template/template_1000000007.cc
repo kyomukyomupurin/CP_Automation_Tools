d3f78c83342996fde0e0b97d9fff7a408596e365
kyomukyomupurin <murakami1728@gmail.com>
2021-05-02 22:53:46 +0900

    :recycle: Refactor code

--- src/.template/template_1000000007.cc
+++ src/.template/template_1000000007.cc
@@ -7,8 +7,8 @@ using int64 = long long;
 #define ALL(v) std::begin(v), std::end(v)
 #define RALL(v) std::rbegin(v), std::rend(v)
 
-template <class T> inline bool upmin(T& x, const T y) { if (x > y) { x = y; return 1; } return 0; }
-template <class T> inline bool upmax(T& x, const T y) { if (x < y) { x = y; return 1; } return 0; }
+template <class T> inline bool upmin(T& x, T y) { if (x > y) { x = y; return 1; } return 0; }
+template <class T> inline bool upmax(T& x, T y) { if (x < y) { x = y; return 1; } return 0; }
 
 template <class T>
 T inverse(T a, T m) {
