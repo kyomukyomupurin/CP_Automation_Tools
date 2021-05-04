e90efde39fd8672ef6425fc6992be28a59880aed
kyomukyomupurin <murakami1728@gmail.com>
2021-05-04 15:25:52 +0900

    :recycle: Refactor code

--- src/.template/template_YESNO.cc
+++ src/.template/template_YESNO.cc
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 using int64 = long long;
 
 #define ALL(v) std::begin(v), std::end(v)
 #define RALL(v) std::rbegin(v), std::rend(v)
 
-template <class T> inline bool upmin(T& x, T y) { if (x > y) { x = y; return 1; } return 0; }
-template <class T> inline bool upmax(T& x, T y) { if (x < y) { x = y; return 1; } return 0; }
+template <class T> inline bool minimize(T& x, T y) { if (x > y) { x = y; return 1; } return 0; }
+template <class T> inline bool maximize(T& x, T y) { if (x < y) { x = y; return 1; } return 0; }
 inline void YES(bool cond) noexcept { std::cout << (cond ? "YES" : "NO") << '\n'; }
 
 int main() {
--- src/.template/template_YesNo.cc
+++ src/.template/template_YesNo.cc
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 using int64 = long long;
 
 #define ALL(v) std::begin(v), std::end(v)
 #define RALL(v) std::rbegin(v), std::rend(v)
 
-template <class T> inline bool upmin(T& x, T y) { if (x > y) { x = y; return 1; } return 0; }
-template <class T> inline bool upmax(T& x, T y) { if (x < y) { x = y; return 1; } return 0; }
+template <class T> inline bool minimize(T& x, T y) { if (x > y) { x = y; return 1; } return 0; }
+template <class T> inline bool maximize(T& x, T y) { if (x < y) { x = y; return 1; } return 0; }
 inline void Yes(bool cond) noexcept { std::cout << (cond ? "Yes" : "No") << '\n'; }
 
 int main() {
