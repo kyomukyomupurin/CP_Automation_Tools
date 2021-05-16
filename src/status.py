55611fb43729418a6e522f19005394a47458ea70
kyomukyomupurin <murakami1728@gmail.com>
2021-05-16 17:10:29 +0900

    :recycle: Refactor code

--- src/status.py
+++ src/status.py
@@ -36,11 +36,18 @@ def get_status() -> None:
     for tr in tbody.find_all("tr"):
         submission_info: list[str] = [td.get_text(strip=True) for td in tr.find_all(
             "td") if len(td.find_all("a")) < 2 and td.get_text(strip=True) != "Detail"]
-        (submission_time, task, language, score, code_size,
-            status, exec_time, memory) = submission_info
-        status_color = Color.GREEN if status == "AC" else Color.YELLOW
-        print(
-            f" {submission_time[:-5]}   {task[0]}   {language:>15}   {score:>4}   {code_size:>11}   {status_color}{status:>3}{Color.DEFAULT}   {exec_time:>7}   {memory:>9}")
+        flag_CE: bool = len(submission_info) == 6
+        if flag_CE:
+            (submission_time, task, language, score, code_size,
+             status) = submission_info
+            print(
+                f" {submission_time[:-5]}   {task[0]}   {language:>15}   {score:>4}   {code_size:>11}   {Color.YELLOW}{status:>3}{Color.DEFAULT}")
+        else:
+            (submission_time, task, language, score, code_size,
+             status, exec_time, memory) = submission_info
+            status_color = Color.GREEN if status == "AC" else Color.YELLOW
+            print(
+                f" {submission_time[:-5]}   {task[0]}   {language:>15}   {score:>4}   {code_size:>11}   {status_color}{status:>3}{Color.DEFAULT}   {exec_time:>7}   {memory:>9}")
 
 
 if __name__ == "__main__":
